7b1763622809a5516668fcb75a44e5cde117d4da
Gustavo Eloi <gustavooeloi@gmail.com>
2025-08-25 14:31:01 -0300

    controller: create ticket

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,3 +1,19 @@
+import { randomUUID } from "node:crypto";
+
 export function create({ request, response }) {
-  return response.end("Criado com sucesso!");
+  const { equipment, description, user_name } = request.body;
+
+  const ticket = {
+    id: randomUUID(),
+    equipment,
+    description,
+    user_name,
+    status: "open",
+    created_at: new Date(),
+    updated_at: new Date(),
+  };
+
+  return response.writeHead(201).end(JSON.stringify(ticket));
 }
+
+// id (uuid), equipment, description, user_name, status, created_at, updated_at
